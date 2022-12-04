b2dda2f19b1ec851aca2575d56750f39d767c426
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-04 13:59:59 +0300

    Example012_Method

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор Зуев С.Н.");
+}
+Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+// Вид 2.1
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Текст",4);
+// или Method21(count: 4, msg: "новый текст");
+
+//Вид 3:
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
